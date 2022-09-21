738e10a74d930563d1771fe4e2f32f2e7c016f4e
ezuykow <zuy1996@gmail.com>
2022-09-21 14:23:25 +0300

    remove ApplicationContext.xml

--- src/main/java/ru/ezuykow/Main.java
+++ src/main/java/ru/ezuykow/Main.java
@@ -1,11 +1,12 @@
 package ru.ezuykow;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.ezuykow.springConfig.SpringConfig;
 
 public class Main {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context =
-                new ClassPathXmlApplicationContext("ApplicationContext.xml");
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(SpringConfig.class);
 
         Computer computer = context.getBean("computer", Computer.class);
         System.out.println(computer);
---
+++ src/main/java/ru/ezuykow/springConfig/SpringConfig.java
@@ -0,0 +1,9 @@
+package ru.ezuykow.springConfig;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("ru.ezuykow")
+public class SpringConfig {
+}
